d70a169ed43c0abcaec90d6dc6dca1cc9c839e66
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-09-19 16:32:18 -0300

    Tratamento do erro 400 Bad Request

--- src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exception
 
 import br.com.alura.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -42,6 +43,26 @@ import javax.servlet.http.HttpServletRequest
                 return ErrorView(
                     status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
                     error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+                    message = exception.message ,
+                    path = request.servletPath
+
+                )
+            }
+
+    
+
+
+            @ExceptionHandler (MethodArgumentNotValidException::class)
+            @ResponseStatus(HttpStatus.BAD_REQUEST)
+            fun handleValidationError(
+                exception: MethodArgumentNotValidException ,
+                request: HttpServletRequest
+
+            ): ErrorView {
+
+                return ErrorView(
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    error = HttpStatus.BAD_REQUEST.name,
                     message = exception.message ,
                     path = request.servletPath
 
